7db40a4ac81af8ddf2358b08348fd4d2309e4ca6
Atieh <atieh.saberi@gmail.com>
2025-03-21 12:42:26 -0400

    OOP connected n game

---
+++ src/main/java/oop/connectedn/game/Game.java
@@ -0,0 +1,38 @@
+package oop.connectedn.game;
+
+import oop.connectedn.grid.Grid;
+
+public class Game {
+    private int maxScore = 0;
+    private int target;
+    private Grid grid;
+
+    private int getMaxScore(int score1, int score2) {
+        return Math.max(score1, score2);
+    }
+
+    private void horizentalMove() {
+
+    }
+
+    private void verticalMove() {
+
+    }
+
+    private void diagonmalMove() {
+
+    }
+
+    private void play(Grid grid) {
+        this.grid = grid;
+
+    }
+
+    /*private Player getWinner() {
+        for(int i = 0; i < )
+
+        return
+    }*/
+
+
+}
